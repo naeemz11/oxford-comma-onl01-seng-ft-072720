c94827ceec15cf742dddcf7a5c3bbeccc74d9256
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-30 18:37:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
 
+#def oxford_comma(array)
+#if array.length == 1 
+  
+#end
+#end
+
+array = ["apple"]
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
+
+
